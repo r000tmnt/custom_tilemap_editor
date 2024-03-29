761b7ed10c9526b9eed669e1ba32dbaea295b2ee
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-03-29 11:27:37 +0800

    [Create] API delete level data

---
+++ server/api/data.delete.ts
@@ -0,0 +1,19 @@
+import fs from 'node:fs';
+
+export default defineEventHandler(async(event) => {
+    const body = await readBody(event)
+
+    console.log("body :>>> ", body)
+
+    const { id } = body
+
+    try {
+        fs.unlinkSync(`${process.env.DATA_PATH}/${id}.js`)
+
+        return { status: 200 }
+    } catch (error) {
+        console.log(error)
+
+        return { status: 500 }
+    }
+})
