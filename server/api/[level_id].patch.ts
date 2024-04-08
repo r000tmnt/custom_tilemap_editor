231bf492900339aaf691e51e2eee8b048cda8817
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-08 15:53:17 +0800

    [Create] API, level_id patch ( update )

---
+++ server/api/[level_id].patch.ts
@@ -0,0 +1,21 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+export default defineEventHandler( async(event) => {
+    const body = await readBody(event)
+
+    console.log("body :>>> ", body)
+
+    try{
+        const filePath = path.join(process.cwd(), `${process.env.DATA_PATH}`,`${body.id}.json`)
+
+        fs.appendFileSync(filePath, JSON.stringify(body))
+
+        return { status: 200 }
+    }catch(err){
+        console.log(err)
+        return { status: 500, err }
+    }
+
+
+})
