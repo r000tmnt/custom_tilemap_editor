16834a0f4e3d1794e203cc5132069c4b77654978
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-09 09:40:46 +0800

    [Update] Extract the app bar in editor as a component

---
+++ components/editorToolBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app-bar id="bar" :elevation="5">
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>{{ levelData.id }}</v-app-bar-title>
+
+        <template v-slot:append>
+            <v-btn icon="mdi-navigation" :color="mode === 'nav'? 'primary': 'grey'" @click="mode = 'nav'"></v-btn>
+
+            <v-btn icon="mdi-pencil" :color="mode === 'draw'? 'primary': 'grey'" @click="mode = 'draw'"></v-btn>
+
+            <v-btn icon="mdi-eraser" :color="mode === 'erase'? 'primary': 'grey'" @click="mode = 'erase'"></v-btn>
+
+            <v-btn icon="mdi-layers" :color="mode === 'layer'? 'primary': 'grey'" @click="mode = 'layer'"></v-btn>
+
+            <v-btn icon="mdi-cog" :color="mode === 'config'? 'primary': 'grey'" @click="mode = 'config'"></v-btn>
+        </template>
+    </v-app-bar>  
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+const { levelData, mode } = storeToRefs(useEditorStore())
+</script>
