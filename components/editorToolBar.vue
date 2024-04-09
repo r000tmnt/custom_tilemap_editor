1a3778726d61ea6e90761f001917dcc046ce926a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-09 13:51:33 +0800

    [Update] Place the tools at the center of app bar

--- components/editorToolBar.vue
+++ components/editorToolBar.vue
@@ -1,27 +1,48 @@
 <template>
-    <v-app-bar id="bar" :elevation="5">
-        <template v-slot:prepend>
+    <v-app-bar id="bar" :elevation="5" style="position:relative">
+        <!-- <template v-slot:prepend>
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
+        </template> -->
 
         <v-app-bar-title>{{ levelData.id }}</v-app-bar-title>
 
+        <div>
+            <v-toolbar style="position: absolute; width:fit-content; margin: auto; top: 0;left: 50%; transform: translateX(-50%)">
+                <v-btn icon="mdi-navigation" :color="mode === 'nav'? 'primary': 'grey'" @click="mode = 'nav'"></v-btn>
+
+                <v-btn icon="mdi-pencil" :color="mode === 'draw'? 'primary': 'grey'" @click="mode = 'draw'"></v-btn>
+
+                <v-btn icon="mdi-eraser" :color="mode === 'erase'? 'primary': 'grey'" @click="mode = 'erase'"></v-btn>
+
+                <v-menu>
+                    <template v-slot:activator="{ props }">
+                        <v-btn icon="mdi-layers" v-bind="props"></v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item
+                        v-for="(item, index) in layers"
+                        :key="index"
+                        :value="index"
+                        >
+                        <v-list-item-title>{{ item }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                 </v-menu>
+            </v-toolbar>
+        </div>
+
         <template v-slot:append>
-            <v-btn icon="mdi-navigation" :color="mode === 'nav'? 'primary': 'grey'" @click="mode = 'nav'"></v-btn>
-
-            <v-btn icon="mdi-pencil" :color="mode === 'draw'? 'primary': 'grey'" @click="mode = 'draw'"></v-btn>
-
-            <v-btn icon="mdi-eraser" :color="mode === 'erase'? 'primary': 'grey'" @click="mode = 'erase'"></v-btn>
-
-            <v-btn icon="mdi-layers" :color="mode === 'layer'? 'primary': 'grey'" @click="mode = 'layer'"></v-btn>
-
-            <v-btn icon="mdi-cog" :color="mode === 'config'? 'primary': 'grey'" @click="mode = 'config'"></v-btn>
+            <v-btn icon="mdi-export"></v-btn>
+            <v-btn icon="mdi-cog"></v-btn>
         </template>
     </v-app-bar>  
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { storeToRefs } from 'pinia'
+
+const layers = ref<string[]>(['map', 'character', 'event'])
 
 const { levelData, mode } = storeToRefs(useEditorStore())
 </script>
