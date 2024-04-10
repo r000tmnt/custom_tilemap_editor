07b23622b59b04ff09e5d895629502b24c91fbdd
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-10 18:04:03 +0800

    [Update] Add navigation drawer to the layput

--- layouts/base.vue
+++ layouts/base.vue
@@ -1,29 +1,37 @@
 <template>
     <v-app>
-        <v-theme-provider id="theme" class="pa-10" theme="dark" with-background>
-            <!-- <v-navigation-drawer v-if="!hide">
-                <v-list-item link title="List Item 1"></v-list-item>
-                <v-list-item link title="List Item 2"></v-list-item>
-                <v-list-item link title="List Item 3"></v-list-item>
-            </v-navigation-drawer> -->
+        <v-theme-provider id="theme" theme="dark" with-background>
+            <v-layout>
+                <v-app-bar id="bar" :elevation="5">
+                    <template v-slot:prepend>
+                        <v-app-bar-nav-icon @click="hide = !hide"></v-app-bar-nav-icon>
+                    </template>
 
-            <v-app-bar id="bar" :elevation="5">
-                <template v-slot:prepend>
-                    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-                </template>
+                    <v-app-bar-title>Application Bar</v-app-bar-title>
 
-                <v-app-bar-title>Application Bar</v-app-bar-title>
+                    <!-- <template v-slot:append>
+                        <v-btn icon="mdi-heart"></v-btn>
 
-                <!-- <template v-slot:append>
-                    <v-btn icon="mdi-heart"></v-btn>
+                        <v-btn icon="mdi-magnify"></v-btn>
 
-                    <v-btn icon="mdi-magnify"></v-btn>
+                        <v-btn icon="mdi-dots-vertical"></v-btn>
+                    </template> -->
+                </v-app-bar>   
 
-                    <v-btn icon="mdi-dots-vertical"></v-btn>
-                </template> -->
-            </v-app-bar>   
-            
-            <slot />            
+                <v-navigation-drawer v-if="!hide">
+                    <v-list-item link prepend-icon="mdi-list-box" title="Level">
+                        <Nuxt-link to="/"></Nuxt-link>
+                    </v-list-item>
+                    <v-list-item link prepend-icon="mdi-treasure-chest-outline" title="ITEM"></v-list-item>
+                    <v-list-item link prepend-icon="mdi-magic-staff" title="SKILL"></v-list-item>
+                </v-navigation-drawer>
+
+                <v-main>
+                    <slot />  
+                </v-main>                
+            </v-layout>
+
+
         </v-theme-provider>
 
     </v-app>
@@ -31,9 +39,9 @@
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue'
+import { ref } from 'vue'
 
-// const hide = ref(false)
+const hide = ref(false)
 
 </script>
 
