ddf41c589436316e33bb5a705fa8c045a3ddb60e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-10 15:03:25 +0800

    [Create] Extract tileInfo as a component

---
+++ components/editorTileInfo.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card class="info" subtitle="Tile info" :text="`Width:${props.width}\nHeight:${props.height}\nX:${tileInfo.x}\nY:${tileInfo.y}\nEvents:${tileInfo.events.length}`">
+        <v-card-actions>
+            <v-btn color="primary" @click="createEvent(tileInfo.x, tileInfo.y)">Add event</v-btn>
+            <v-btn :color="(tileInfo.events.length)? 'secondary' : 'grey'" :disabled="tileInfo.events.length === 0">Manage events</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
+const props = defineProps(
+    {
+        width: {
+            type: Number,
+            default: 0
+        },
+        height: {
+            type: Number,
+            default: 0
+        },
+    }
+)
+
+const { tileInfo, levelData } = storeToRefs(useEditorStore())
+const { toggleDialog } = useDialogStore()
+
+const createEvent = (x: number, y: number) => {
+    const newEvent = {
+        position: { x, y },
+        item: [],
+        scene: []
+    }
+    levelData.value.event.push(newEvent)
+    tileInfo.value.events.push(newEvent)
+
+    toggleDialog("event-create")
+}
+</script>
