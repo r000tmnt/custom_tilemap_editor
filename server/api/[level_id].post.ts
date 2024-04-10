9822f8c65366a6c7ef94b8e15fd2c07e726e4c82
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-10 13:09:47 +0800

    [Update] Combine levelData from request body

--- server/api/[level_id].post.ts
+++ server/api/[level_id].post.ts
@@ -6,75 +6,14 @@ export default defineEventHandler( async(event) => {
 
     console.log("body :>>> ", body)
 
-    const { codeName, name, width, height } = body
-
-    // console.log(`${process.env.URL}${folder}`)
-
-    const newMap = []
-
-    for(let i=0; i < Number(height); i++){
-        const row : number[] = []
-        newMap.push(row)
-
-        for(let j=0; j < Number(width); j++){
-            newMap[i].push(0)
-        }
-    }
-
     try{
         const newLevel = `import { t } from '../../utils/i18n'
 
         export default {
-            "id": "${codeName}",
-            "name": "${name}",
-            "map": ${JSON.stringify(newMap)},
-            "audio": "", // Audio for battle phase
-            "assets": [ // define in numeric order
-                "",
-                "",
-                "",
-                "",
-                ""
-            ],
-            // conversation - 對話階段, 
-            // battle - 戰鬥階段
-            // intermission - 整備階段
-            // end - 回到標題畫面
-            "phase": [ 'conversation', 'titleCard', 'battle', 'conversation', 'end' ],
-            "event": [], // Pre-defined events
-            "enemy": [],
-            "objective": {
-                // In what condition does player clear the level
-                // Clear all, pass X turns, Defeat XXX...etc
-                "victory": {
-                    "target": "enemy",
-                    "value": 0
-                },
-                // In what condition does player lose the level
-                // Defeat all, XXX down...etc
-                "fail": {
-                    "target": "player",
-                    "value": 0
-                },
-                // In what condition does player get bonus
-                "optional": [
-                    {
-                        "target": "turn",
-                        "value": 6,
-                        "prize": [
-                            { // Prize for clear the level
-                                "id": 'currency_1',
-                                "type": 1,
-                                "amount": 100
-                            }
-                        ]
-                    }
-                ]
-            },
-            "difficulty": 1
+            ${body}
         }`
 
-        const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}`,`${codeName}.js`)
+        const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}`,`${body.id}.js`)
 
         fs.appendFileSync(filePath, newLevel)
 
