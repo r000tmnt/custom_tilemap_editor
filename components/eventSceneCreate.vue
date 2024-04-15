f3db842877ea514cb31625426e4b62430785d63f
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-15 17:52:49 +0800

    [WIP] Event scene create form

--- components/eventSceneCreate.vue
+++ components/eventSceneCreate.vue
@@ -14,12 +14,24 @@
         <v-form @submit.prevent>
             <v-container>
                 <!-- Background image file -->
+                <v-select label="Background image"></v-select>
 
                 <!-- Aduio file -->
+                <v-select label="Background audio"></v-select>
 
                 <!-- Max people on the screen, default to 1 -->
+                <v-text-field type="number"></v-text-field>
 
                 <!-- dialogue tree -->
+                <v-card-actions>
+                    <v-btn>New dialogue</v-btn>
+                </v-card-actions>
+                <v-expansion-panels v-for="(scene, index) in tileInfo.events[tileInfo.events.length - 1].scene.length" :key="index">
+                    <v-expansion-panel>
+                        <v-expansion-panel-title>{{ `SCENE ${index + 1}` }}</v-expansion-panel-title>
+
+                    </v-expansion-panel>
+                </v-expansion-panels>
             </v-container>
         </v-form>
       </v-card>
@@ -28,7 +40,12 @@
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue'
+// import { eventDialogueModel } from '~/types/level'
 
 const { toggleDialog } = useDialogStore()
 const { eventSceneCreateDialog } = storeToRefs(useDialogStore())
+const { tileInfo, editEventIndex } = storeToRefs(useEditorStore())
+
+// const newDialogues = ref<eventDialogueModel>([])
 </script>
