666dd256d14d0b3ade0b536f4d9529d3617d6b0a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-15 17:05:53 +0800

    [Create] Event scene create dialog

---
+++ components/eventSceneCreate.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-dialog
+      v-model="eventSceneCreateDialog"
+      width="auto"
+      persistent
+    >
+      <v-card
+        class="pa-2"
+        width="800"
+        max-height="1000"
+        :scrollable="true"
+        title="Create new event"
+      >
+        <v-form @submit.prevent>
+            <v-container>
+                <!-- Background image file -->
+
+                <!-- Aduio file -->
+
+                <!-- Max people on the screen, default to 1 -->
+
+                <!-- dialogue tree -->
+            </v-container>
+        </v-form>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
+const { toggleDialog } = useDialogStore()
+const { eventSceneCreateDialog } = storeToRefs(useDialogStore())
+</script>
