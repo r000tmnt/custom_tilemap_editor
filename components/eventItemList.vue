a72cf433e7749fc2166f61f7f2d588df2b2e5e34
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-15 14:41:52 +0800

    [Update] Select an item to set quantity

--- components/eventItemList.vue
+++ components/eventItemList.vue
@@ -2,25 +2,48 @@
     <v-dialog
       v-model="eventItemDialog"
       width="auto"
+      persistent
     >
         <v-card
-            class="pa-2"
+            class="pa-4"
             width="800"
             max-height="1000"
             :scrollable="true"
             title="Choose items for the event"
         >
-            <v-list>
-                <v-list-item
-                    v-for="(value, name, index) in item">
-                    <v-card :title="name"
-                        v-for="(val, pointer) in value">
-                            <div @click="selectItem(val)">
-                                {{ val }}
+            <v-item-group selected-class="bg-light-blue-lighten-5" :multiple="true">
+                <v-item v-for="(value, name, index) in item" 
+                    :key="name"
+                    class="mt-4"
+                    v-slot="{ isSelected, selectedClass, toggle }">
+                    <span>{{ name }}</span>
+                    <v-card v-for="(val, pointer) in value"
+                        :key="pointer"
+                        class="mb-4"
+                        :class="[selectedClass]"
+                        @click="selectItem(val)">
+                            <div class="px-4 my-4 d-flex justify-space-between" @click="toggle">
+                                <span>{{ val.name }}</span>
+
+                                <div class="d-flex w-25" v-if="isSelected">
+                                <!-- <v-icon icon="mdi-minus"></v-icon> -->
+                                <v-text-field
+                                    type="number" 
+                                    label="Quantity" 
+                                    bg-color="white" 
+                                    @click.stop=""
+                                    @input="(e: any) => setItemAmount(e, val.id)"></v-text-field>
+                                <!-- <v-icon icon="mdi-plus"></v-icon> -->
+                                </div>
                             </div>
                     </v-card>
-                </v-list-item>
-            </v-list>    
+                </v-item>
+            </v-item-group>    
+            
+            <div class="d-flex justify-end">
+                <v-btn class="mr-2 bg-grey" @click="toggleDialog('event-item')">Cancel</v-btn>
+                <v-btn color="primary" @click="updateEvent">Confirm</v-btn>
+            </div>
         </v-card>
     </v-dialog>
 </template>
@@ -29,26 +52,55 @@
 import { onBeforeMount, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import type { materialDataModel, otherDataModel, potionDataModel, armorDataModel, weaponDataModel, keyDataModel } from "~/types/item";
+import type { eventItemModel } from "~/types/level"
 
 
 const { item } = storeToRefs(useItemStore())
 const { getItemData } = useItemStore()
 const { eventItemDialog } = storeToRefs(useDialogStore())
+const { toggleDialog } = useDialogStore()
 
-const selectedItem = ref<materialDataModel[] | otherDataModel[] | potionDataModel[] | armorDataModel[] | weaponDataModel[] | keyDataModel[]>([])
+const emit = defineEmits(["eventItemUpdate"])
 
-const selectItem = (val: any) => {
+const selectedItem = ref<eventItemModel[]>([])
+
+const selectItem = (val: materialDataModel | otherDataModel | potionDataModel | armorDataModel | weaponDataModel | keyDataModel) => {
+    console.log(val)
+
     const exist = selectedItem.value.findIndex(s => s.id === val.id)
     if(exist >= 0){
         // Cancel selection
         selectedItem.value.splice(exist, 1)
     }else{
         // Add selection
-        selectedItem.value.push(val)
+        selectedItem.value.push({
+            amount: 1,
+            id: val.id,
+            type: val.type
+        })
     }
+}
+
+const setItemAmount = (e: any, id:string) => {
+    console.log(e, id)
+
+    const exist = selectedItem.value.findIndex(s => s.id === id)
+    if(exist >= 0){
+        // Update selected item
+        selectedItem.value[exist] = e.target.value
+    }
+}
+
+const updateEvent = () => {
+    emit("eventItemUpdate", selectedItem.value)
+    toggleDialog("event-item")
 }
 
 onBeforeMount(() => {
     getItemData()
 })
 </script>
+
+<!-- <style scoped>
+
+</style> -->
