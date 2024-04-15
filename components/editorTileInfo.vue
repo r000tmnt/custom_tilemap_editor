c06f4eb8d6d3f9c20d3fb23483669354a88ad302
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-15 14:38:39 +0800

    [WIP] List the events on the bottom of tile info

--- components/editorTileInfo.vue
+++ components/editorTileInfo.vue
@@ -2,8 +2,14 @@
     <v-card class="info" subtitle="Tile info" :text="`Width:${props.width}\nHeight:${props.height}\nX:${tileInfo.x}\nY:${tileInfo.y}\nEvents:${tileInfo.events.length}`">
         <v-card-actions>
             <v-btn color="primary" @click="createEvent(tileInfo.x, tileInfo.y)">Add event</v-btn>
-            <v-btn :color="(tileInfo.events.length)? 'secondary' : 'grey'" :disabled="tileInfo.events.length === 0">Manage events</v-btn>
+            <!-- <v-btn :color="(tileInfo.events.length)? 'secondary' : 'grey'" :disabled="tileInfo.events.length === 0">Manage events</v-btn> -->
         </v-card-actions>
+
+        <v-list>
+            <v-list-item v-for="(event, index) in tileInfo.events" :key="index">
+                <v-card :text="`Item: ${event.item.length} Scene: ${event.scene.length} Trigger: ${event.trigger}`"></v-card>
+            </v-list-item>
+        </v-list>
     </v-card>
 </template>
 
@@ -30,7 +36,8 @@ const createEvent = (x: number, y: number) => {
     const newEvent = {
         position: { x, y },
         item: [],
-        scene: []
+        scene: [],
+        trigger: 'auto'
     }
     levelData.value.event.push(newEvent)
     tileInfo.value.events.push(newEvent)
