a37e6f157c40cddca0b6acf092e2f77f9df73c45
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-16 13:22:58 +0800

    [Create] API asset animation get

---
+++ server/api/asset/animation.get.ts
@@ -0,0 +1,16 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+export default defineEventHandler(async(event) => {
+    const query = getQuery(event)
+
+    console.log(query)
+
+    const { type } = query
+
+    const relativePath = path.relative(process.cwd(), "./public")
+    const files = fs.readdirSync(`${relativePath}/assets/images/${type}/animation`)
+    const assets = files.map(file => `/assets/images/${type}/animation/${file}`)
+
+    return { status: 200, assets }
+})
