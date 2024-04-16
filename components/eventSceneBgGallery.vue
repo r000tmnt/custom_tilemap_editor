816d3cecf189b5047706433a2038120e1a372677
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-16 15:30:52 +0800

    [Create] Event scene bg gallery component

---
+++ components/eventSceneBgGallery.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-dialog
+      v-model="bgAssetsGalleryDialog"
+      width="auto"
+      persistent
+    >
+        <v-card
+            class="pa-2"
+            width="800"
+            max-height="1000"
+            :scrollable="true"
+            title="Background images"
+        >
+            <v-row>
+                <v-col v-for="path in assets.bg" :key="path" 
+                cols="4">
+                    <v-img :src="path"
+                        :width="300"
+                        :max-height="300"
+                        cover
+                        @click="selectBackground(path)"></v-img>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+const { assets } = storeToRefs(useEditorStore())
+const { bgAssetsGalleryDialog } = storeToRefs(useDialogStore())
+const { toggleDialog } = useDialogStore()
+
+const emit = defineEmits(["setSceneBackGround"])
+
+const selectBackground = (path: string) => {
+    emit("setSceneBackGround", path)
+    toggleDialog("scene-bg")
+}
+</script>
+
