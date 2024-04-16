381ee1a833558e662f3ec576a9f036ad32bd471d
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-16 18:11:14 +0800

    [WIP] eventOptionCreate component

---
+++ components/eventOptionCreate.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-dialog
+      v-model="eventSceneCreateDialog"
+      width="auto"
+      persistent
+    >
+      <v-card
+        class="pa-2"
+        width="800"
+        max-height="1000"
+        :scrollable="true"
+        title="Create event option"
+      ></v-card>
+        <v-row>
+            <v-col>
+                <!-- Value -->
+                <v-text-field label="Value"></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <!-- Content -->
+                <v-textarea label="Content"></v-textarea>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <!-- audio (optional) -->
+                <v-select title="click audio"
+                        :items="audioAssets.general"></v-select>
+            </v-col>
+        </v-row>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+const { audioAssets } = storeToRefs(useEditorStore())
+</script>
