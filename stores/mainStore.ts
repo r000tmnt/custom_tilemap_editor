3597a1a9bb18d3c5a69bec438ac725094ac8b848
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-17 10:02:09 +0800

    [Update] Combine protocol and host to form the base url

--- stores/mainStore.ts
+++ stores/mainStore.ts
@@ -2,9 +2,9 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useMainStore = defineStore('main', () => {
-    const runtimeConfig = useRuntimeConfig()
+    const url = useRequestURL()
 
-    const base_url = ref<string>(runtimeConfig.public.URL)
+    const base_url = ref<string>(`${url.protocol}//${url.host}/`)
 
     const tileSize = ref<number>(32)
 
