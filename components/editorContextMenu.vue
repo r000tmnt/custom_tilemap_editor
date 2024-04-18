3fe353091cc93a6a6c4fc41524228b635a4a3040
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-18 18:11:57 +0800

    [WIP] Custom context menu

---
+++ components/editorContextMenu.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-list>
+        <v-list-group value="Set starting point">
+            <v-list-item>Player</v-list-item>
+            <v-list-item>Enemy</v-list-item>
+        </v-list-group>
+        <v-list-group value="Remove starting point">
+            <v-list-item>Player</v-list-item>
+            <v-list-item>Enemy</v-list-item>
+        </v-list-group>
+        <v-list-item>Clear tiles on the map</v-list-item>
+        <v-list-item>Expand map</v-list-item>
+    </v-list>
+</template>
