dd33e9a47e8b078523ca9efd17ba4644c2742b74
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-19 13:57:11 +0800

    [Update] Change contextmenu position on props update

--- components/editorContextMenu.vue
+++ components/editorContextMenu.vue
@@ -1,14 +1,47 @@
 <template>
-    <v-list>
+    <v-list v-if="contextMenu" id="contextMenu" ref="contextRef">
         <v-list-group value="Set starting point">
             <v-list-item>Player</v-list-item>
             <v-list-item>Enemy</v-list-item>
         </v-list-group>
-        <v-list-group value="Remove starting point">
-            <v-list-item>Player</v-list-item>
-            <v-list-item>Enemy</v-list-item>
-        </v-list-group>
+        <v-list-item>Remove starting point</v-list-item>
         <v-list-item>Clear tiles on the map</v-list-item>
         <v-list-item>Expand map</v-list-item>
     </v-list>
 </template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { storeToRefs } from 'pinia';
+
+const { contextMenu } = storeToRefs(useDialogStore())
+
+const props = defineProps({
+    x: {
+        type: Number,
+        default: 0
+    },
+    y: {
+        type: Number,
+        default: 0
+    },
+})
+
+const contextRef = ref()
+
+watch(() => [ props.x, props.y ], (newPosition) => {
+    // console.log(newPosition)
+    if(contextRef.value){
+        const contextMenuElement = contextRef.value.$el
+        contextMenuElement.style.top = newPosition[1] + 'px'
+        contextMenuElement.style.left = newPosition[0] + 'px'
+        console.log(contextMenuElement)
+    }
+})
+</script>
+
+<style>
+#contextMenu{
+    position: absolute;
+}
+</style>
