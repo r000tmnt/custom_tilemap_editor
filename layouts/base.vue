257ba2a84182032bdbc346b96631ccde5e2120b1
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-23 10:36:45 +0800

    [Update] Adjust base layout

--- layouts/base.vue
+++ layouts/base.vue
@@ -2,6 +2,18 @@
     <v-theme-provider id="theme" theme="dark" with-background>
         <v-app>
             <v-layout>
+                <v-navigation-drawer v-if="!hide">
+                    <Nuxt-link to="/">
+                        <v-list-item link prepend-icon="mdi-list-box" title="Level"></v-list-item>
+                    </Nuxt-link>
+                    
+                    <Nuxt-link to="/item">
+                        <v-list-item link prepend-icon="mdi-treasure-chest-outline" title="ITEM"></v-list-item>
+                    </Nuxt-link>
+
+                    <v-list-item link prepend-icon="mdi-magic-staff" title="SKILL"></v-list-item>
+                </v-navigation-drawer>
+
                 <v-app-bar id="bar" :elevation="5">
                     <template v-slot:prepend>
                         <v-app-bar-nav-icon @click="hide = !hide"></v-app-bar-nav-icon>
@@ -17,18 +29,6 @@
                         <v-btn icon="mdi-dots-vertical"></v-btn>
                     </template> -->
                 </v-app-bar>   
-
-                <v-navigation-drawer v-if="!hide">
-                    <Nuxt-link to="/">
-                        <v-list-item link prepend-icon="mdi-list-box" title="Level"></v-list-item>
-                    </Nuxt-link>
-                    
-                    <Nuxt-link to="/item">
-                        <v-list-item link prepend-icon="mdi-treasure-chest-outline" title="ITEM"></v-list-item>
-                    </Nuxt-link>
-
-                    <v-list-item link prepend-icon="mdi-magic-staff" title="SKILL"></v-list-item>
-                </v-navigation-drawer>
 
                 <v-main class="pageContent">
                     <slot />  
