1c3828d00d160f23a3f95778915ddb43b266a19c
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-23 10:36:17 +0800

    [Create] Custom useFetch

---
+++ composables/useCustomFetch.ts
@@ -0,0 +1,29 @@
+export default async function $api(
+  url: string,
+  options = {}
+) {
+  return await useFetch(url, {
+    ...options,
+    // watch: false,
+    // immediate: false,
+    onRequest({ request, options }) {
+      // Set the request headers
+      // options.headers = options.headers || {}
+      // options.headers.authorization = '...'
+      const header = options.headers? new Headers(options.headers) : new Headers()
+
+      options.headers = header
+    },
+    onRequestError({ request, options, error }) {
+      // Handle the request errors
+      console.log(error)
+    },
+    onResponse({ request, response, options }) {
+      // Process the response data
+      localStorage.setItem('token', response._data.token)
+    },
+    onResponseError({ request, response, options }) {
+      // Handle the response errors
+    },
+  })
+}
