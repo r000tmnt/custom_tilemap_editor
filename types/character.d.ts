1b5b37a197ea63201ff717499a8c15b46105bdcc
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-23 15:30:04 +0800

    [Create] Character data model interface

---
+++ types/character.d.ts
@@ -0,0 +1,38 @@
+import type { baseAttributeModel } from './item'
+import type responseModel from './serverResponse'
+
+interface characterDataModel {
+    id: string
+    name: string
+    prefer_attributes: string[],
+    base_attribute: baseAttributeModel,
+    skill: string[]
+}
+
+interface itemDropModel {
+    id: string
+    amount: number
+    type: number
+    rate: number
+}
+
+interface classDataModel extends characterDataModel {
+    bag: itemDropModel[]
+}
+
+interface mobDataModel extends characterDataModel {
+    prefer_action: string
+    prefer_skill: string,
+    drop: itemDropModel[]
+}
+
+interface mobResponseModel extends responseModel {
+    data: mobDataModel[]
+}
+
+export{
+    itemDropModel,
+    classDataModel,
+    mobDataModel,
+    mobResponseModel
+}
