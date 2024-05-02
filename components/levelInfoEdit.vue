9d874a5f60f73ae1bee49d6b7a6c28d32fa4e9e9
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-02 11:50:34 +0800

    [WIP] Switch to edit mode for level objectives

--- components/levelInfoEdit.vue
+++ components/levelInfoEdit.vue
@@ -9,7 +9,7 @@
         width="500"
         title="Edit level info"
       >
-        <v-form ref="formRef">
+        <v-form ref="formRef" class="pt-4 px-5">
             <v-text-field label="Level name"
                 :rules="inputRules"
                 v-model="newName"></v-text-field>
@@ -28,11 +28,19 @@
                   </tmplate>
 
                   <template v-else>
-                    <v-select label="Target"></v-select>
-                    <v-text-field label="value" type="numer"></v-text-field>
-                    <v-select label="Prize"></v-select>
+                    <v-select label="Target" 
+                      v-model="levelData.objective.victory.target"
+                      :items="objectiveTarget"
+                      @update:model-value="setValueLimit"
+                    ></v-select>
+                    <v-text-field label="value" type="numer" v-model="levelData.objective.victory.value"></v-text-field>
+                    <!-- <v-select label="Prize"></v-select> -->
 
-                    <v-btn icon="mdi-check" @click="editObjective = ''"></v-btn>
+                    <div class="d-flex justify-end">
+                      <v-icon icon="mdi-close" color="grey" class="mr-2" @click="editObjective = ''"></v-icon>
+                      <v-icon icon="mdi-check" color="primary" @click="editObjective = ''"></v-icon>
+                    </div>
+                    
                   </template>
                 </v-list-item>
                 <v-list-item title="Fail">
@@ -45,10 +53,10 @@
                   </tmplate>
 
                   <template v-else>
-                    <v-select label="Target"></v-select>
-                    <v-text-field label="value" type="numer"></v-text-field>
+                    <v-select label="Target" v-model="levelData.objective.fail.target"></v-select>
+                    <v-text-field label="value" type="numer" v-model="levelData.objective.fail.value"></v-text-field>
 
-                    <v-btn icon="mdi-check" @click="editObjective = ''"></v-btn>
+                    <v-icon icon="mdi-check" color="primary" @click="editObjective = ''"></v-icon>
                   </template>
                 </v-list-item>
                 <v-list-item title="Optional">
@@ -61,13 +69,13 @@
                             <span>
                               {{ `Target: ${option.target}\nValue: ${option.value}` }}
                             </span>
+                            <v-icon class="ml-2" color="secondary" icon="mdi-note-edit-outline" @click="editObjective = 'optional'"></v-icon>
                           </v-list-item>                        
                         </template>
 
                         {{ `Prize: ` }}
                         <v-list-item v-for="bonus in option.prize">
                           {{ `Id: ${bonus.id}\nAmount: ${bonus.amount}` }}
-                          <v-icon color="secondary" icon="mdi-note-edit-outline" @click="editObjective = 'optional'"></v-icon>
                         </v-list-item>
                       </v-list-group>
                   </tmplate>
@@ -78,17 +86,18 @@
                         value="Prize">
                         <template v-slot:activator="{ props }">
                           <v-list-item v-bind="props">
-                            <v-select label="Target"></v-select>
-                            <v-text-field label="Value" type="number"></v-text-field>
+                            <v-select label="Target" v-model="option.target"></v-select>
+                            <v-text-field label="Value" type="number" v-model="option.value"></v-text-field>
                           </v-list-item>                        
                         </template>
                         
                         {{ `Prize: ` }}
                         <v-list-item v-for="bonus in option.prize">
-                          <v-select label="prize"></v-select>
-                          <v-text-field label="Value" type="number"></v-text-field>
+                          <v-select label="prize" v-model="bonus.id"></v-select>
+                          <v-text-field label="Value" type="number" v-model="bonus.amount"></v-text-field>
                           <v-icon color="secondary" icon="mdi-note-edit-outline" @click="editObjective = 'optional'"></v-icon>
                         </v-list-item>
+                        <v-icon icon="mdi-plus"></v-icon>
                       </v-list-group>
 
                     <v-btn icon="mdi-check" @click="editObjective = ''"></v-btn>
@@ -118,6 +127,28 @@ const { inputRules } = useRuleStore()
 const newName = ref<string>(`${levelData.value.name}`)
 const formRef = ref()
 const editObjective = ref<string>("")
+const objectiveTarget = ref<string[]>([
+  "player",
+  "enemy",
+  "turn"
+])
+
+const targetLimit = ref<number>(0)
+
+const setValueLimit = (v: any) => {
+  console.log(v)
+  switch(v){
+    case 'player':
+      targetLimit.value = levelData.value.player.length - 1
+    break;
+    case 'enemy':
+      targetLimit.value = levelData.value.enemy.length - 1
+    break;
+    case 'turn':
+      targetLimit.value = Infinity
+    break;
+  }
+}
 
 const changeLevelName = () => {
     formRef.value?.validate().then((result: any) => {
