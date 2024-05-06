50bc2a337ac9310efb3e3bd3cd32e2e20116f4ff
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-06 11:05:10 +0800

    [Update] New state attributes and statusList

--- stores/characterStore.ts
+++ stores/characterStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import type { classDataModel, mobDataModel, mobResponseModel } from "~/types/character";
+import type { baseAttributeModel } from "~/types/item";
 
 export const useCharacterStore = defineStore('character', () => {
     const classes = ref<classDataModel[]>([])
@@ -8,6 +9,27 @@ export const useCharacterStore = defineStore('character', () => {
     const mobs = ref<mobDataModel[]>([])
 
     const mainStore = useMainStore()
+
+    const attributes = ref<baseAttributeModel>({
+        hp: 0,
+        mp: 0,
+        ap: 0,
+        str: 0,
+        def: 0,
+        int: 0,
+        spd: 0,
+        spi: 0,
+        lck: 0,
+        maxHp: 0,
+        maxMp: 0,
+        maxAp: 0,
+        moveSpeed: 0,
+        sight: 0,
+    })
+
+    const statusList = ref<string[]>([
+        "Burn", "Poison", "Freeze", "Stone", "Sleep", "Bleed"
+    ])
 
     const getMobData = async() => {
         try {
@@ -24,6 +46,8 @@ export const useCharacterStore = defineStore('character', () => {
     return {
         classes,
         mobs,
+        attributes,
+        statusList,
         getMobData
     }
 })
