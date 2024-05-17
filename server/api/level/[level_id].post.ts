79a711da8ecec5d388613d0affc6cc3f6bce5785
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-17 13:28:51 +0800

    [Update] level data out put with level collector

--- server/api/level/[level_id].post.ts
+++ server/api/level/[level_id].post.ts
@@ -6,16 +6,70 @@ export default defineEventHandler( async(event) => {
 
     // console.log("body :>>> ", body)
 
+    const { levels } = event.context.formidable.fields
+
     try{
-        const newLevel = `import { t } from '../../utils/i18n'
+        for(let i=0; i < levels.length; i++){
+            const newLevel = `import { t } from '../../utils/i18n'
 
+            export default {
+                ${levels[i]}
+            }`     
+            
+            const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/level/`,`${levels[i].id}.js`)
+            fs.appendFileSync(filePath, newLevel)
+        }
+
+
+        const levelCollector = `
+        ${`/**
+        * Place the conversation event at the beginning of the event array and state the trigger value as "auto"
+        * Other event like drop item or character interation are place after conversation event
+        */`}
         export default {
-            ${event.context.formidable.fields}
-        }`
+            data: [],
+            
+            getAll(){
+                return this.data
+            },
 
-        const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/level/`,`${event.context.formidable.fields.id}.js`)
+            getOne(id){
+                return this.data.find(k => k.id === id)
+            },
 
-        fs.appendFileSync(filePath, newLevel)
+            search(keyWord){
+                return this.data.filter(k => k.name.includes(keyWord))
+            },
+
+            getTile(id, row, col){
+                const map = this.data.find(l => l.id === id).map
+        
+                return map[row][col]
+            },
+
+            ${`/**
+            * Loading level data from the pointed path
+            * @param {string} id - The id of the level 
+            */`}
+            async load(id){
+                const index = this.data.findIndex(k => k.id === id)
+                const levelData = await import(${"`" + "./level/" + "${" + "id" + "}" + ".js" + "`"})
+        
+                console.log('level data:>>> ', levelData)
+        
+                if(index >= 0){
+                    this.data[index] = {...levelData.default}
+                }else{
+                    this.data.push({...levelData.default})
+                }
+        
+                console.log('level :>>>', this.data)
+            }
+        }
+        `
+
+        const levelCollectorPath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/`,"level.js")
+        fs.appendFileSync(levelCollectorPath, levelCollector)
 
         return { status: 200 }
     }catch(err){
