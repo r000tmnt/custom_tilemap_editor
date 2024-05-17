8606644c768f585040388abd84b964aa9e437a32
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-17 14:06:59 +0800

    [Update] Dispaly asset type with card component

--- pages/asset.vue
+++ pages/asset.vue
@@ -1,13 +1,31 @@
 <template>
-    <v-list>
-        <v-list-item v-for="type in assetType"
-            @click="selectType(type)">
-            {{ type }}
-        </v-list-item>
-    </v-list>
+    <section id="wrapper">
+        <div class="mr-auto">
+            <h3>ASSETS</h3>
+        </div>
+        
+        <div class="d-flex flex-wrap p-2 w-75 mx-auto">
+            <v-row>
+                <v-col v-for="type in assetType"
+                    @click="selectType(type)"
+                    cols="4">
+                    <v-card :title="type">
+                        <template v-slot:actions>
+                            <v-btn color="secondary" text="VIEW"></v-btn>
+                        </template>
+                    </v-card>
+                </v-col>                
+            </v-row>
+
+        </div>        
+    </section>
 </template>
 
 <script setup lang="ts" >
+definePageMeta({
+    layout: 'base'
+})
+
 import { storeToRefs } from 'pinia';
 import { ref, onBeforeMount, onMounted } from 'vue'
 import type { levelAssetModel } from '~/types/level';
@@ -15,7 +33,7 @@ import type { levelAssetModel } from '~/types/level';
 const { assets } = storeToRefs(useEditorStore())
 const { getImagesAssets, getAudioAssets, getBattleAudioAsset } = useEditorStore()
 
-const assetType = ref(Object.entries(assets).map(a => a[0]))
+const assetType = ref(Object.entries(assets.value).map(a => a[0]))
 
 const assetToDisplay = ref<any[]>([])
 
