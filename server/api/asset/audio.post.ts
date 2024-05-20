234b3b88b6cb381576609c4cf228d33bef5c91de
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-20 13:59:23 +0800

    [Create] Api audio post

---
+++ server/api/asset/audio.post.ts
@@ -0,0 +1,31 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+export default defineEventHandler(async(event) => {
+    const { fields, files } = event.context.formidable;
+    
+    console.log("fields :>>>", fields)
+    console.log("files :>>>", files)
+    
+    // const body = await readBody(event)
+
+    // console.log(body)
+
+    const { type } = fields
+
+    files.forEach((file: any) => {
+        console.log(file)
+        const filePath = path.join(process.cwd(), `./public/assets/audio/${type}`)
+
+        fs.writeFileSync(filePath, file)
+    });
+
+    
+
+    // const relativePath = path.relative(process.cwd(), "./public")
+    // const files = fs.readdirSync(`${relativePath}/assets/images/${type}`)
+    // const assets = files.map(file => `/assets/images/${type}/${file}`)
+
+    // return { status: 200, assets: assets.filter(a => a.includes(".png")) }
+    return { status: 200 }
+})
