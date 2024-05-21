5d27b397bad0a89f19cdb8940e47051f4d738465
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-21 10:42:24 +0800

    [Create] lang page

---
+++ pages/lang.vue
@@ -0,0 +1,52 @@
+<template>
+    <section id="wrapper">
+        <div class="mr-auto">
+            <h3>TRANSLATIONS</h3>
+        </div>
+        
+        <div class="d-flex flex-wrap p-2 w-75 mx-auto">
+            <v-row>
+                <v-col v-for="type in translateTargets"
+                    :key="type"
+                    cols="4">
+                    <v-card :title="type">
+                        <template v-slot:actions>
+                            <v-btn color="secondary" 
+                                text="VIEW"
+                                @click="selectType(type)"></v-btn>
+                        </template>
+                    </v-card>
+                </v-col>                
+            </v-row>
+        </div> 
+        
+    </section>
+</template>
+
+<script setup lang="ts" >
+definePageMeta({
+    layout: 'base'
+})
+
+import { storeToRefs } from 'pinia';
+import { ref, onBeforeMount, onMounted } from 'vue'
+
+const { translateTargets } = storeToRefs(useLangStore())
+const { toggleDialog } = useDialogStore()
+
+const assetToDisplay = ref<any[]>([])
+const selectedType = ref<string>("")
+
+const selectType = (type: string) => {
+    const translationPath = `../locale/${selectedType.value}`
+    // TODO - Get message inside locale directory
+}
+
+onMounted(() => {
+
+}) 
+
+onBeforeMount(() => {
+
+})
+</script>
