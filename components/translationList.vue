ac48545aff79219fafde5dd2b554b801f16557f2
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-22 15:23:17 +0800

    [Update] Display both en and zh translation

--- components/translationList.vue
+++ components/translationList.vue
@@ -10,15 +10,40 @@
     :title="`${props.type} translation`"
   >
     <template v-slot:text>
-      <v-list-item v-for="item in translationData[props.type as keyof translationDataModel]">
-          {{ item.split(".")[0] }}
-      </v-list-item>      
+      <template v-if="Object.entries(translationDetail).length">
+        <v-form class="d-flex">
+            <!-- en message & zh message  -->
+            <div class="w-50">
+                <p>EN</p>
+                <div v-for="(value, key, index) in translationDetail.en"
+                  class="d-flex my-2 pa-2"
+                  :key="key">
+                  <div class="px-2" style="white-space: pre-line;">{{ String(key).includes("option")? `${key}: ${value.value}\nrespond: ${value.content}` : `${key}: ${value}` }}</div>
+                  <v-btn variant="outlined" color="secondary" class="ml-auto">EDIT</v-btn>
+                </div>
+            </div>
+
+            <div class="w-50">
+                <p>ZH</p>
+                <div v-for="(value, key, index) in translationDetail.zh"
+                  class="d-flex my-2 pa-2"
+                  :key="key">
+                  <div class="px-2" style="white-space: pre-line;">{{ String(key).includes("option")? `${key}: ${value.value}\nrespond: ${value.content}` : `${key}: ${value}` }}</div>
+                  <v-btn variant="outlined" color="secondary" class="ml-auto">EDIT</v-btn>
+                </div>
+            </div>
+        </v-form>
+      </template>
+
+      <template v-else>
+        <v-list-item v-for="item in translationData[props.type as keyof translationDataModel]"
+          :key="item"
+          @click="getTranslation(item)">
+            {{ item.split(".")[0] }}
+        </v-list-item>           
+      </template>
     </template>
-    <!--<div class="d-flex">
-         en message & zh message 
-        <v-data-table></v-data-table>
-        <v-data-table></v-data-table>
-    </div>-->
+
     <v-card-actions>
         <v-btn @click="toggleDialog('translation-viewer')">CLOSE</v-btn>
     </v-card-actions>
@@ -28,12 +53,13 @@
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
-import { ref } from 'vue';
+import { onBeforeUnmount } from 'vue';
 import type { translationDataModel } from '~/types/level';
 
-const { translationData } = storeToRefs(useLangStore())
+const { translationData, translationDetail } = storeToRefs(useLangStore())
 const { translationListDialog } = storeToRefs(useDialogStore())
 const { toggleDialog } = useDialogStore()
+const { getTranslationData } = useLangStore()
 
 const props = defineProps({
     type: {
@@ -42,4 +68,13 @@ const props = defineProps({
     }
 })
 
+const getTranslation = (item: string) => {
+  // Get the tanslated string from locale
+  getTranslationData(item, props.type)
+}
+
+// Reset translation detail
+onBeforeUnmount(() => {
+  translationDetail.value = {}
+})
 </script>
