865ea39064b2e49cde951e01aa77a603a82b192a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-22 14:43:50 +0800

    [Update] Return the content of both en and zh json file

---
+++ server/api/lang/data.get.ts
@@ -0,0 +1,26 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+export default defineEventHandler((event) => {
+    const query = getQuery(event)
+
+    console.log(query)
+
+    const { type, name } = query
+
+    // Get both en and zh tranlation
+    try{
+        const enFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).replace(".json", "_en.json")}`)
+        const zhFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).replace(".json", "_zh.json")}`)
+        
+        const enData = fs.readFileSync(enFilePath, { encoding: 'utf-8' })
+        const zhData = fs.readFileSync(zhFilePath, { encoding: 'utf-8' })
+
+        console.log("data:>>> ", enData)
+        console.log("data:>>> ", zhData)
+
+        return { status: 200, data: { en: JSON.parse(enData), zh: JSON.parse(zhData) } }       
+    }catch(err){
+        return { status: 500, err }
+    }
+})
