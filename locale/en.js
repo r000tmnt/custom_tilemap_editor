128c7978115434467096cac2e9455d07f0cbe81a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-22 14:43:13 +0800

    [Update] Import en and zh translation from json file

--- locale/en.js
+++ locale/en.js
@@ -1,32 +1,32 @@
-import tutorial_1 from './level/tutorial_1_en'
-import ui from './ui/ui_en'
-import potion from './item/item_potion_en'
-import weapon from './item/item_weapon_en'
-import armor from './item/item_armor_en'
-import material from './item/item_material_en'
-import key from './item/item_key_en'
-import other from './item/item_other_en'
-import skill_sword from './skill/sword_en'
-import buff from './skill/buff_en'
-import debuff from './skill/debuff_en'
-import job from './class/class_en'
+import tutorial_1 from './level/tutorial_1_en.json'
+import ui from './ui/ui_en.json'
+import potion from './item/item_potion_en.json'
+import weapon from './item/item_weapon_en.json'
+import armor from './item/item_armor_en.json'
+import material from './item/item_material_en.json'
+import key from './item/item_key_en.json'
+import other from './item/item_other_en.json'
+import skill_sword from './skill/sword_en.json'
+import buff from './skill/buff_en.json'
+import debuff from './skill/debuff_en.json'
+import job from './class/class_en.json'
 
 export default {
     tutorial_1,
     ui,
     job,
     "item": {
-        ...potion,
-        ...weapon,
-        ...armor,
-        ...material,
-        ...key,
-        ...other
+        potion,
+        weapon,
+        armor,
+        material,
+        key,
+        other
     },
     "skill":{
-        ...skill_sword,
-        ...buff,
-        ...debuff
+        skill_sword,
+        buff,
+        debuff
     },
     "demo": "This is the end of the demo.\nThank you for playing the game.",
     "loading": "Loading...",
--- locale/zh.js
+++ locale/zh.js
@@ -1,32 +1,32 @@
-import tutorial_1 from './level/tutorial_1_zh'
-import ui from './ui/ui_zh'
-import potion from './item/item_potion_zh'
-import weapon from './item/item_weapon_zh'
-import armor from './item/item_armor_zh'
-import material from './item/item_material_zh'
-import key from './item/item_key_zh'
-import other from './item/item_other_zh'
-import skill_sword from './skill/sword_zh'
-import buff from './skill/buff_zh'
-import debuff from './skill/debuff_zh'
-import job from './class/class_zh'
+import tutorial_1 from './level/tutorial_1_zh.json'
+import ui from './ui/ui_zh.json'
+import potion from './item/item_potion_zh.json'
+import weapon from './item/item_weapon_zh.json'
+import armor from './item/item_armor_zh.json'
+import material from './item/item_material_zh.json'
+import key from './item/item_key_zh.json'
+import other from './item/item_other_zh.json'
+import skill_sword from './skill/sword_zh.json'
+import buff from './skill/buff_zh.json'
+import debuff from './skill/debuff_zh.json'
+import job from './class/class_zh.json'
 
 export default {
     job,
     tutorial_1,
     ui,
     "item": {
-        ...potion,
-        ...weapon,
-        ...armor,
-        ...material,
-        ...key,
-        ...other
+        potion,
+        weapon,
+        armor,
+        material,
+        key,
+        other
     },
     "skill":{
-        ...skill_sword,
-        ...buff,
-        ...debuff
+        skill_sword,
+        buff,
+        debuff
     },
     "demo": "這裡是 DEMO 的終點。 感謝你的遊玩",
     "loading": "讀取中...",
