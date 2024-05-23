222a23c037fd6ad371391cba25129131b61d403c
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-23 14:06:01 +0800

    [Update] api mob get

--- server/api/mob.get.ts
+++ server/api/mob.get.ts
@@ -1,9 +1,14 @@
 import fs from 'node:fs';
 // import path from 'node:path';
 
-export default defineEventHandler(() => {
+export default defineEventHandler((event) => {
+    const query = getQuery(event)
+
+    console.log(query)
+
+    const { target } = query
     try{
-        const mobs = fs.readFileSync(`${process.env.DATA_PATH}/mobs.json`, { encoding: 'utf-8' })
+        const mobs = fs.readFileSync(`${process.env.DATA_PATH}/mob/mob_${target}.json`, { encoding: 'utf-8' })
 
         console.log(mobs)
 
