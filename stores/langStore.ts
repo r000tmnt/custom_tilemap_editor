ba868da12229ff69071c757410b542630674f7b0
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-23 14:07:56 +0800

    [WIP] If the locale data is not found, create an temporary state for new locale data

--- stores/langStore.ts
+++ stores/langStore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import type { levelList, translationDataModel, levelDataResponse } from '~/types/level';
+import type { levelList, translationDataModel, levelDataResponse, levelEventModel } from '~/types/level';
 
 // Define mainStore
 const mainStore = useMainStore()
@@ -40,6 +40,52 @@ export const useLangStore = defineStore('lang', () => {
 
         if(tranlationRequest.status === 200){
             translationDetail.value = tranlationRequest.data
+        }else{
+            // Need to create a new json file
+            let rawDataRequest : any = {}
+            switch(type){
+                case 'class':
+                    rawDataRequest = target.includes('class')? await $fetch(`${mainStore.base_url}api/class?target=${target}`) : await $fetch(`${mainStore.base_url}api/mob?target=${target}`)
+                break;
+                case 'item':
+                    rawDataRequest = await $fetch(`${mainStore.base_url}api/item/${type}/`)
+
+                    translationDetail.value.en = { name: rawDataRequest.data.name }
+                    translationDetail.value.zh = { name: "" } 
+                break;
+                case 'level':
+                    rawDataRequest = await $fetch(`${mainStore.base_url}api/level/${target}`)
+
+                    translationDetail.value.en.title = rawDataRequest.data.tile
+
+                    const eventWithOutPosition = rawDataRequest.data.event.filter((e:levelEventModel) => !Object.entries(e.position).length)
+
+                    if(eventWithOutPosition.length){
+                        let dialogueCount = 1, optionCount = 1
+                        for(let i=0; i < eventWithOutPosition.length; i++){
+                            for(let j=0, scene = eventWithOutPosition[i].scene; j < scene.length; j++){
+                                for(let k=0, dialogue = scene[j].dialogue; k < dialogue.length; k++){
+                                    if(dialogue[k].option && dialogue[k].option.length){
+                                        for(let l=0, option = dialogue[k].option; l < option.length; l++){
+                                            translationDetail.value.en[`option_${optionCount}`].value = option[l].value
+                                            translationDetail.value.en[`option_${optionCount}`].content = option[l].contnet
+
+                                            optionCount += 1
+                                        }
+                                    }else{
+                                        translationDetail.value.en[`dialogue_${dialogueCount}`] = dialogue[k].content
+
+                                        dialogueCount += 1
+                                    }
+                                }
+                            }
+                        }
+                    }
+                break;
+                case 'skill':
+
+                break;
+            }
         }
     }
 
