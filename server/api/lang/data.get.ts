586f87833c52e4b55981a4cad81ff0a328bae240
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-23 14:07:12 +0800

    [Update] Get the locale data based on the requesting type and name

--- server/api/lang/data.get.ts
+++ server/api/lang/data.get.ts
@@ -8,10 +8,38 @@ export default defineEventHandler((event) => {
 
     const { type, name } = query
 
-    // Get both en and zh tranlation
+    // Get both en and zh translation
     try{
-        const enFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).replace(".json", "_en.json")}`)
-        const zhFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).replace(".json", "_zh.json")}`)
+        let enFilePath = "", zhFilePath = ""
+
+        switch(type){
+            case "class":
+                enFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).split("_")[0]}_en.json`)
+                zhFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).split("_")[0]}_zh.json`)
+            break;
+            case "item": case "level":
+                enFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).replace(".json", "_en.json")}`)
+                zhFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).replace(".json", "_zh.json")}`)
+            break;
+            case "skill":
+                // And more...
+                const statusKeyWord = {
+                    buff: /(foucs)/,
+                    debuff: /(poison)/
+                }
+
+                if(statusKeyWord.buff.test(String(name))){
+                    enFilePath = path.join(process.cwd(), `locale/${type}`, "buff_en.json")
+                    zhFilePath = path.join(process.cwd(), `locale/${type}`, "buff_zh.json")
+                }else if(statusKeyWord.debuff.test(String(name))){
+                    enFilePath = path.join(process.cwd(), `locale/${type}`, "debuff_en.json")
+                    zhFilePath = path.join(process.cwd(), `locale/${type}`, "debuff_zh.json")
+                }else{
+                    enFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).split("_")[1]}_en.json`)
+                    zhFilePath = path.join(process.cwd(), `locale/${type}`, `${String(name).split("_")[1]}_zh.json`)
+                }
+            break; 
+        }
         
         const enData = fs.readFileSync(enFilePath, { encoding: 'utf-8' })
         const zhData = fs.readFileSync(zhFilePath, { encoding: 'utf-8' })
