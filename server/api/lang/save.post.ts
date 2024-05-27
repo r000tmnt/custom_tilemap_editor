a0e70e89e1a27ac1948321fcf12bc7f6057d2b23
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-27 13:46:51 +0800

    [Create] api lang save

---
+++ server/api/lang/save.post.ts
@@ -0,0 +1,30 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+export default defineEventHandler( async(event) => {
+    // console.log("POST :>>>", event)
+
+    // const body = await readBody(event)
+
+    // console.log("body :>>> ", body)
+
+    const { type, name, en, zh } = event.context.formidable.fields
+
+    // console.log(`${process.env.URL}${folder}`)
+
+
+    try{
+        const enFilePath = path.join(process.cwd(), `locale/${type}/`,`${name}_en.json`)
+        const zhFilePath = path.join(process.cwd(), `locale/${type}/`,`${name}_zh.json`)
+
+        fs.appendFileSync(enFilePath, en)
+        fs.appendFileSync(zhFilePath, zh)
+
+        return { status: 200 }
+    }catch(err){
+        console.log(err)
+        return { status: 500, err }
+    }
+
+
+})
