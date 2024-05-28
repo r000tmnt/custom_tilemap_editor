e2994007452b886eaa583028079e23591c8acc6a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-05-28 16:04:58 +0800

    [Update] Return error if failed to read file

--- server/api/asset/animation.get.ts
+++ server/api/asset/animation.get.ts
@@ -8,9 +8,13 @@ export default defineEventHandler(async(event) => {
 
     const { type } = query
 
-    const relativePath = path.relative(process.cwd(), "./public")
-    const files = fs.readdirSync(`${relativePath}/assets/images/${type}/animation`)
-    const assets = files.map(file => `/assets/images/${type}/animation/${file}`)
+    try {
+        const relativePath = path.relative(process.cwd(), "./public")
+        const files = fs.readdirSync(`${relativePath}/assets/images/${type}/animation`)
+        const assets = files.map(file => `/assets/images/${type}/animation/${file}`)
 
-    return { status: 200, assets }
+        return { status: 200, assets }        
+    } catch (error) {
+        return { status: 200, error }  
+    }
 })
