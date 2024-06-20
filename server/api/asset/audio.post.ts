2fa3d76ba4e53d854e94da8b1837b1a209e4aeb3
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-20 15:34:14 +0800

    [WIP] Audio post api

--- server/api/asset/audio.post.ts
+++ server/api/asset/audio.post.ts
@@ -1,31 +1,44 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import formidable from 'formidable';
 
 export default defineEventHandler(async(event) => {
-    const { fields, files } = event.context.formidable;
+    const form = formidable({ multiples: true })
+
+    const result = new Promise((resolve, reject) => {
+        form.parse(event.node.req, (err, fields, files) => {
+            console.log("fields :>>>", fields)
+            // console.log("files :>>>", files)    
+            
+            if(err){
+                reject({ status: 500, error: err })
+            }
+
+            if(files){
+                let fileCount = 0
+
+                const loopFile = () => {
+                    const file = files[String(fileCount)]
+
+                    if(file){
+                        console.log("file :>>>", file[0])
+                        fileCount += 1
+                        loopFile()
+                    }
+                }
+                loopFile()
+            }
+
+            // const { type } = fields
+        })
+    })
     
-    console.log("fields :>>>", fields)
-    console.log("files :>>>", files)
-    
-    // const body = await readBody(event)
 
-    // console.log(body)
+    // files.forEach((file: any) => {
+    //     console.log(file)
+    //     const filePath = path.join(process.cwd(), `./public/assets/audio/${type}`)
 
-    const { type } = fields
-
-    files.forEach((file: any) => {
-        console.log(file)
-        const filePath = path.join(process.cwd(), `./public/assets/audio/${type}`)
-
-        fs.writeFileSync(filePath, file)
-    });
-
-    
-
-    // const relativePath = path.relative(process.cwd(), "./public")
-    // const files = fs.readdirSync(`${relativePath}/assets/images/${type}`)
-    // const assets = files.map(file => `/assets/images/${type}/${file}`)
-
-    // return { status: 200, assets: assets.filter(a => a.includes(".png")) }
+    //     fs.writeFileSync(filePath, file)
+    // });
     return { status: 200 }
 })
