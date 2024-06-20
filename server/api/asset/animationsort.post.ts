e4e530e91d08c1cd76b50bea39c0edf4b1a74bf6
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-20 10:49:07 +0800

    [Update] Rename files in order

--- server/api/asset/animationsort.post.ts
+++ server/api/asset/animationsort.post.ts
@@ -8,10 +8,48 @@ export default defineEventHandler(async(event) => {
 
     const { oldIndex, newIndex, type, oldFileName } = body
 
+    const animationTypeArr = oldFileName.split("_")
+
+    const level = `${animationTypeArr[1]}_${animationTypeArr[2]}_${animationTypeArr[3]}`
+
     try {
-        // const relativePath = path.relative(process.cwd(), "./public")
-        // const files = fs.readdirSync(`${relativePath}/assets/images/${type}/animation`)
-        // const assets = files.map(file => `/assets/images/${type}/animation/${file}`)
+        const relativePath = path.relative(process.cwd(), "./public")
+        const pathPrefix = `${relativePath}/assets/images/${type}/animation`
+        // Get files with simillar name
+        const frames = fs.readdirSync(pathPrefix).filter(files => files.includes(level))
+
+        const fileToExchange1 = `${pathPrefix}/${animationTypeArr[0]}_${animationTypeArr[1]}_${animationTypeArr[2]}_${animationTypeArr[3]}_${newIndex + 1}.png`
+
+        const fileToExchange2 = `${pathPrefix}/${oldFileName}`
+
+        const tempFileName = `${pathPrefix}/${animationTypeArr[0]}_${animationTypeArr[1]}_${animationTypeArr[2]}_${animationTypeArr[3]}_${frames.length + 1}.png`
+
+        // Rename files
+        fs.rename(fileToExchange1, tempFileName, function (err) {
+            if(err){
+                console.log(`Rename ${fileToExchange1} failed :>>>`, err)
+            }else{
+                console.log("Rename first time compelete")
+
+                fs.rename(fileToExchange2, fileToExchange1, function (err) {
+                    if(err){
+                        console.log(`Rename ${oldFileName} failed :>>>`, err)
+                    }else{
+                        console.log("Rename second time compelete")
+
+                        // Rename the first file back to the order
+                        fs.rename(tempFileName, fileToExchange2, function (err) {
+                            if(err){
+                                console.log(`Rename ${tempFileName} failed :>>>`, err)
+                            }else{
+                                console.log("Rename third time compelete")
+                            }
+                        })
+                    }
+                })
+        
+            }
+        })
 
         return { status: 200,  }        
     } catch (error) {
