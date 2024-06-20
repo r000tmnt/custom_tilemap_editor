27bc076febdc7babe43f5a1e12780d818d55b5d6
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-20 15:34:49 +0800

    [Update] Divide upload image and upload animation frame to it's own scope

--- server/api/asset/image.post.ts
+++ server/api/asset/image.post.ts
@@ -1,51 +1,97 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import formidable from 'formidable';
+import getFrame from '~/server/utils/getFrame';
 
 export default defineEventHandler(async(event) => {
     const form = formidable({ multiples: true })
 
     const result = new Promise((resolve, reject) => {
         form.parse(event.node.req, (err, fields, files) => {
-            console.log("fields :>>>", fields)
+            // console.log("fields :>>>", fields)
             // console.log("files :>>>", files)    
             
             if(err){
                 reject({ status: 500, error: err })
             }
 
-            if(files){
-                let fileCount = 0
+            const { type, name } = fields
 
-                const loopFile = () => {
-                    const file = files[String(fileCount)]
+            let fileCount = 0
 
-                    if(file){
-                        // If file Exist
-                        console.log("file :>>>", file[0])
-                        const { filepath, originalFilename } = file[0]
-                        const { type } = fields
-                        const newfilePath = path.join(process.cwd(), `./public/assets/images/${type? type[0]: 'env'}`, originalFilename? originalFilename : `${String(Date.now())}.png`)
+            if(type){
+                if(type[0].includes("animation") && name !== undefined){
+                    console.log("animation")
+                    const pathPrefix = `./public/assets/images/${type[0].split("-")[1]}/animation`
+                    const frames = getFrame(pathPrefix, name[0]).length
 
-                        try {
-                            const rawData = fs.readFileSync(filepath)
-                            fs.writeFile(newfilePath, rawData, function(err){
-                                console.log("write file err:>>>", err)
-                            })
-                            fileCount += 1
-                            loopFile()                            
-                        } catch (error) {
-                            reject({ status: 500, error })
+                    if(files){
+                        const loopFile = () => {
+                            const file = files[String(fileCount)]
+
+                            if(file){
+                                // If file Exist
+                                console.log("file :>>>", file[0])
+                                const { filepath } = file[0]
+
+                                // Rename file
+                                const newfilePath = path.join(process.cwd(), pathPrefix, `${name}_${frames + (fileCount + 1)}.png`)
+
+                                try {
+                                    const rawData = fs.readFileSync(filepath)
+                                    fs.writeFile(newfilePath, rawData, function(err){
+                                        if(err){
+                                            console.log("write file err:>>>", err)
+                                        }
+                                    })
+                                    fileCount += 1
+                                    loopFile()                            
+                                } catch (error) {
+                                    reject({ status: 500, error })
+                                }
+                            }else{
+                                // If file not exist
+                                resolve({ status: 200 })
+                            }
                         }
-                    }else{
-                        // If file not exist
-                        resolve({ status: 200 })
+                        loopFile()
+                    }
+                }else{
+                    if(files){
+                        const loopFile = () => {
+                            const file = files[String(fileCount)]
+        
+                            if(file){
+                                // If file Exist
+                                console.log("file :>>>", file[0])
+                                const { filepath, originalFilename } = file[0]
+        
+                                console.log("image")
+                                const pathPrefix = `./public/assets/images/${type[0]}`
+    
+                                const newfilePath = path.join(process.cwd(), pathPrefix, originalFilename? originalFilename : `${String(Date.now())}.png`)
+    
+                                try {
+                                    const rawData = fs.readFileSync(filepath)
+                                    fs.writeFile(newfilePath, rawData, function(err){
+                                        if(err){
+                                            console.log("write file err:>>>", err)
+                                        }
+                                    })
+                                    fileCount += 1
+                                    loopFile()                            
+                                } catch (error) {
+                                    reject({ status: 500, error })
+                                }
+                            }else{
+                                // If file not exist
+                                resolve({ status: 200 })
+                            }
+                        }
+                        loopFile()
                     }
                 }
-                loopFile()
             }
-
-            // const { type } = fields
         })
     })
 
