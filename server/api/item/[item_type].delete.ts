6e7facc5804da822ee98add43d85a2e5114a63f7
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-24 17:35:24 +0800

    [WIP] Item data output api

---
+++ server/api/item/[item_type].delete.ts
@@ -0,0 +1,21 @@
+import fs from 'node:fs';
+
+export default defineEventHandler(async(event) => {
+    console.log("DELETE :>>>", event.context)
+    const type = await readBody(event)
+
+    console.log("body :>>> ", type)
+
+    // const type = event.context.formidable.fields
+
+    try {
+        // Delete item data
+        fs.unlinkSync(`${process.env.DATA_PATH}/item/item_${type}.json`)
+
+        return { status: 200 }
+    } catch (error) {
+        console.log(error)
+
+        return { status: 500 }
+    }
+})
