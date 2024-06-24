f65c8f73de44cd8c9cd2a7ec21f1fd8a3dc1dbef
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-24 14:33:04 +0800

    [Update] New interface animationCreateModel & animationResponseModel

--- types/animation.d.ts
+++ types/animation.d.ts
@@ -1,3 +1,5 @@
+import { animationSort } from '~/types/animation';
+import type responseModel from './serverResponse';
 interface animation {
     attack?: string[]
     down?: string[]
@@ -19,8 +21,22 @@ interface animationEditData {
     name: string
 }
 
+interface animationCreateModel {
+    type: string
+    target: string
+    level: number
+    name: string
+    frames: File[]
+}
+
+interface animationResponseModel extends responseModel {
+    error?: {}
+}
+
 export {
     animation,
     animationSort,
-    animationEditData
+    animationEditData,
+    animationCreateModel,
+    animationResponseModel
 }
