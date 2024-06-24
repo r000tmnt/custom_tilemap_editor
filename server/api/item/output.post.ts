98c3a9f6d8b0c9cd2bb186024c3ea89f82dfdac0
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-24 16:58:51 +0800

    [WIP] Item data output api

---
+++ server/api/item/output.post.ts
@@ -0,0 +1,44 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+export default defineEventHandler( async(event) => {
+    const body = await readBody(event)
+
+    console.log("body :>>> ", body)
+
+    // const { items } = event.context.formidable.fields
+    const { items, type } = body
+
+    try{
+        for(let i=0; i < items.length; i++){
+            const newLevel = `import { t } from '../../utils/i18n'
+
+            export default {
+                data: [${items[i]}]
+            },
+
+            getAll(){
+                return this.data
+            },
+
+            getOne(id){
+                return this.data.find(a => a.id === id)
+            },
+
+            search(keyWord){
+                return this.data.filter(a => a.name.includes(keyWord))
+            }
+            `     
+            
+            const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/item/`,`item_${type}.js`)
+            fs.appendFileSync(filePath, newLevel)
+        }
+
+        return { status: 200 }
+    }catch(err){
+        console.log(err)
+        return { status: 500, err }
+    }
+
+
+})
