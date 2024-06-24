b9fbe1d6e837e610cbfb0072035225cb9fe4b423
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-24 10:00:14 +0800

    [FIX] api mob get & api class get

--- server/api/class.get.ts
+++ server/api/class.get.ts
@@ -1,18 +1,29 @@
 import fs from 'node:fs';
+import { classDataModel } from '~/types/character';
 // import path from 'node:path';
 
 export default defineEventHandler((event) => {
-    const query = getQuery(event)
+    // const query = getQuery(event)
 
-    console.log(query)
+    // console.log(query)
 
-    const { target} = query
+    // const { target} = query
     try{
-        const data = fs.readFileSync(`${process.env.DATA_PATH}/class/class_${target}.json`, { encoding: 'utf-8' })
+        const pathPrefix = `${process.env.DATA_PATH}/class/`
+
+        const files = fs.readdirSync(pathPrefix)
+
+        const data : classDataModel[] = []
+
+        for(let i=0; i < files.length; i++){
+            const job = fs.readFileSync(`${pathPrefix}${files[i]}`, { encoding: 'utf-8' })
+
+            data.push(JSON.parse(job))
+        }
 
         console.log(data)
 
-        return { status: 200, data: JSON.parse(data) }
+        return { status: 200, data }
     }catch(err){
         return { status: 500, data: [], err }
     }
--- server/api/mob.get.ts
+++ server/api/mob.get.ts
@@ -1,18 +1,29 @@
 import fs from 'node:fs';
+import { mobDataModel } from '~/types/character';
 // import path from 'node:path';
 
 export default defineEventHandler((event) => {
-    const query = getQuery(event)
+    // const query = getQuery(event)
 
-    console.log(query)
+    // console.log(query)
 
-    const { target } = query
+    // const { target } = query
     try{
-        const data = fs.readFileSync(`${process.env.DATA_PATH}/mob/mob_${target}.json`, { encoding: 'utf-8' })
+        const pathPrefix = `${process.env.DATA_PATH}/mob/`
+
+        const files = fs.readdirSync(pathPrefix)
+
+        const data : mobDataModel[] = []
+
+        for(let i=0; i < files.length; i++){
+            const mob = fs.readFileSync(`${pathPrefix}${files[i]}`, { encoding: 'utf-8' })
+
+            data.push(JSON.parse(mob))
+        }
 
         console.log(data)
 
-        return { status: 200, data: JSON.parse(data) }
+        return { status: 200, data }
     }catch(err){
         return { status: 500, data: [], err }
     }
