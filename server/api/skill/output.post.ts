1ba8676133079a952220f001d1a69f09ce03ac5e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-25 12:01:15 +0800

    [Update] Skill collector file format

--- server/api/skill/output.post.ts
+++ server/api/skill/output.post.ts
@@ -21,34 +21,46 @@ export default defineEventHandler( async(event) => {
         for(let i=0; i < skill.length; i++){
             const content = fs.readFileSync(`${pathPrefix}${skill[i]}`, { encoding: "utf-8" })
 
-            const newLevel = `export default {
-                data: ${content}
+            const newSkill = `export default {
+                ${content}
             }
             `     
             
             const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/skill/`, skill[i])
-            fs.appendFileSync(filePath, newLevel)
+            fs.appendFileSync(filePath, newSkill)
         }
 
         // Generate the collector file
         try{
-            for(let i=0; i < skill.length; i++){
-                const newLevel = `import skill_sword from "./skill_sword"
-                import skill_status from "./skill_status"
-                import skill_knife from "./skill_knife"
-                import skill_type from "./skill_type"
+            const skillCollector = `import skill_sword from "./skill_sword"
+            import skill_status from "./skill_status"
+            import skill_knife from "./skill_knife"
+            import skill_type from "./skill_type"
 
-                export default {
+            export default {
+                data: [
                     ...skill_sword,
                     ...skill_status,
                     ...skill_knife,
                     ...skill_type
+                ],
+
+                getAll(){
+                    return this.data
+                },
+
+                getOne(id){
+                    return this.data.find(s => s.id === id)
+                },
+
+                search(keyWord){
+                    return this.data.filter(s => s.name.includes(keyWord))
                 }
-                `     
-                
-                const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/`,`skill.js`)
-                fs.appendFileSync(filePath, newLevel)
             }
+            `     
+            
+            const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/`,`skill.js`)
+            fs.appendFileSync(filePath, skillCollector)
 
             return { status: 200 }
         }catch(error){
