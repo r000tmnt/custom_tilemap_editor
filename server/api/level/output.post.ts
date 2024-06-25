143fbb2802555b46a9c9dfb4131348287abaa392
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-25 10:50:03 +0800

    [Update] Level data output api

--- server/api/level/output.post.ts
+++ server/api/level/output.post.ts
@@ -2,12 +2,10 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 export default defineEventHandler( async(event) => {
-    const body = await readBody(event)
+    const pathPrefix = `${process.env.DATA_PATH}/level/`
+    const levels = fs.readdirSync(pathPrefix)
 
-    console.log("body :>>> ", body)
-
-    // const { levels } = event.context.formidable.fields
-    const { levels } = body
+    console.log(levels)
 
     // Check if folder exist
     if(!fs.existsSync(`${process.env.OUTPUT_PATH}/dataBase`)){
@@ -18,19 +16,21 @@ export default defineEventHandler( async(event) => {
         fs.mkdirSync(`${process.env.OUTPUT_PATH}/dataBase/level/`)
     }
 
+
     try{
         for(let i=0; i < levels.length; i++){
+            const content = fs.readFileSync(`${pathPrefix}${levels[i]}`, { encoding: "utf-8" })
             const newLevel = `import { t } from '../../utils/i18n'
 
             export default {
-                ${levels[i]}
+                ${content}
             }`     
                         
-            const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/level/`,`${levels[i].id}.js`)
+            const filePath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/level/`, levels[i])
             fs.appendFileSync(filePath, newLevel)
         }
 
-
+        // Generate level collector
         const levelCollector = `
         ${`/**
         * Place the conversation event at the beginning of the event array and state the trigger value as "auto"
@@ -79,13 +79,39 @@ export default defineEventHandler( async(event) => {
         `
 
         const levelCollectorPath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/`,"level.js")
-        fs.appendFileSync(levelCollectorPath, levelCollector)
 
-        return { status: 200 }
-    }catch(err){
-        console.log(err)
-        return { status: 500, err }
+        try {
+            fs.appendFileSync(levelCollectorPath, levelCollector)
+
+            // Generate level list
+            const levelList = levels.map(lv => {
+                if(!lv.includes("test")){
+                    return lv.split(".json")
+                }
+            })
+
+            const levelListPath = path.join(process.cwd(), `${process.env.OUTPUT_PATH}/dataBase/`,"levelList.js")
+        
+            console.log("Level list :>>>", levelList)
+
+            const file = `export default {
+                data: ${levelList}
+            }`
+
+            try{
+                fs.appendFileSync(levelListPath, file)
+                return { status: 200 }
+            }catch(error){
+                console.log("Generate level list error :>>>", error)
+                return { status: 500, error }
+            }
+
+        } catch (error) {
+            console.log("Generate level collector error :>>>", error)
+            return { status: 500, error }
+        }
+    }catch(error){
+        console.log("Output level data error :>>>", error)
+        return { status: 500, error }
     }
-
-
 })
