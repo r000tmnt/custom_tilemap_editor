97afff7d5a6448b570ab59adc2f5da874cfc0853
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-26 17:34:10 +0800

    [Update] Adjust layout style

--- pages/lang.vue
+++ pages/lang.vue
@@ -4,7 +4,7 @@
             <h3>TRANSLATIONS</h3>
         </div>
         
-        <div class="d-flex flex-wrap p-2 w-75 mx-auto">
+        <div class="d-flex flex-wrap p-2 w-75 mx-auto mt-4">
             <v-row>
                 <v-col v-for="type in translateTargets"
                     :key="type"
