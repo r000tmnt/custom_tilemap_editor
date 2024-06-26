73d3c4d8fe1ebcded8a36da916e0aaa5c85a0393
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-26 15:06:25 +0800

    [Update] Redesign levelConversationEdit ui with v-list-group

--- components/levelConversationEdit.vue
+++ components/levelConversationEdit.vue
@@ -1,6 +1,6 @@
 <template>
     <v-dialog
-      v-model="levelConversationEdit"
+      v-model="levelConversationEventEdit"
       width="auto"
       persistent
     >
@@ -9,29 +9,44 @@
         width="500"
         title="Edit level conversation phase"
       >
-        <v-btn color="secondary"
-          @click="createConversation">Add Conversation</v-btn>
-
         <v-form v-if="eventBeforeBattle.length" ref="formRef" class="pt-4 px-5">
-                <v-btn color="secondary" @click="toggleDialog('scene-create')">Add Scene</v-btn>
-                <v-list-item 
-                    v-for="(item, index) in levelData.event.filter((e: levelEventModel) => Object.entries(e.position).length === 0)"
-                    :key="index"
-                    @click="editScene(index)">
-                    {{ `${index + 1}. Scene: ${item.scene.length}` }}
-                    <v-icon class="ml-2" color="secondary" icon="mdi-note-edit-outline"></v-icon>
-                </v-list-item> 
+            <v-btn color="secondary"
+            @click="createConversation">Add Conversation</v-btn>
+            
+            <v-list>
+                <v-list-group
+                  v-for="(item, index) in eventBeforeBattle"
+                  :key="index"
+                  :value="`Conversation event ${index + 1}`" @click.stop>
+                  <template v-slot:activator="{ props }">
+                      <v-list-item v-bind="props">
+                        {{ `Conversation event ${index + 1}` }}
+                        <v-icon icon="mdi-trash-can" color="danger" @click="removeEvent(index)"></v-icon>
+                      </v-list-item> 
+                  </template>
+                  <v-btn color="secondary" @click="toggleDialog('scene-create')">Add Scene</v-btn>
+                  
+                  <v-list-item v-for="(content, pointer) in item.scene" >
+                    {{ `Event-${index + 1}-Scene-${pointer + 1}` }}
+                    <v-icon class="ml-2" color="secondary" icon="mdi-note-edit-outline" @click="editScene(pointer)"></v-icon> 
+                    <v-icon icon="mdi-trash-can" color="danger" @click="removeScene(index, pointer)"></v-icon>
+                  </v-list-item>
+                </v-list-group>
+                <!-- <v-icon class="ml-2" icon="mdi-arrow-down" color="secondary"></v-icon> -->
+                <!-- <v-icon class="ml-2" color="secondary" icon="mdi-note-edit-outline" @click="editScene(index)"></v-icon> -->
+                <!-- <v-icon icon="mdi-trash-can" color="danger" @click="eventBeforeBattle.splice(index, 1)"></v-icon> -->
+            </v-list> 
         </v-form>
 
         <div  class="d-flex justify-end">
-            <v-btn color="gray" class="mr-2" @click="cancelEvent">CANCEL</v-btn>
+            <v-btn color="gray" class="mr-2" @click="toggleDialog('level-conversation-edit')">CANCEL</v-btn>
             <v-btn color="primary" @click="createEvent">CONFIRM</v-btn>
         </div>
      </v-card>
     </v-dialog>
 
     <event-scene-create v-if="eventSceneCreateDialog" 
-      @create-scene="createConversationScene" />
+       />
     <event-scene-edit v-if="eventSceneEditDialog" 
       :scene="sceneToEdit"
       @edit-scene="editConversationScene" />
@@ -46,7 +61,7 @@ import eventSceneCreate from './event/eventSceneCreate.vue';
 import eventSceneEdit from './event/eventSceneEdit.vue';
 
 const { levelData } = storeToRefs(useEditorStore())
-const { levelConversationEdit, eventSceneEditDialog, eventSceneCreateDialog } = storeToRefs(useDialogStore())
+const { levelConversationEventEdit, eventSceneEditDialog, eventSceneCreateDialog } = storeToRefs(useDialogStore())
 const { toggleDialog } = useDialogStore()
 const { saveLevelData } = useEditorStore()
 
@@ -55,6 +70,8 @@ const formRef = ref()
 
 const sceneToEdit = ref<eventSceneModel>()
 const editIndex = ref<number>(-1)
+const latestIndex = ref<number>(-1)
+const childIndex = ref<number>(-1)
 
 const eventBeforeBattle = ref<levelEventModel[]>(levelData.value.event.filter(e => !Object.entries(e.position).length))
 
@@ -74,13 +91,6 @@ const createConversation = () => {
     eventBeforeBattle.value.push(newEvent)
 }
 
-const createConversationScene = (v: any) => {
-    console.log(v)
-    const battlePhaseIndex = levelData.value.event.findIndex((e: levelEventModel) => Object.entries(e.position).length)
-    // Insert the event before battlephase
-    levelData.value.event[battlePhaseIndex - 1].scene.push(v)
-}
-
 const editConversationScene = (v: any) => {
     console.log(v)
     const battlePhaseIndex = levelData.value.event.findIndex((e: levelEventModel) => Object.entries(e.position).length)
@@ -95,11 +105,19 @@ const editScene = (index: number) => {
   toggleDialog("scene-edit")
 }
 
-const cancelEvent = () => {
-    const battlePhaseIndex = levelData.value.event.findIndex((e: levelEventModel) => Object.entries(e.position).length)
+const removeEvent = (index: number) => {
+  eventBeforeBattle.value.splice(index, 1)
+  levelData.value.event.splice(index, 1)
+}
 
-    levelData.value.event.splice(battlePhaseIndex - 1, 1)
-    toggleDialog('level-conversation-edit')
+const removeScene = (index: number, pointer: number) => {
+  eventBeforeBattle.value[index].scene.splice(pointer, 1)
+  levelData.value.event[index].scene.splice(pointer, 1)
+}
+
+const prepareIndex = () => {
+  latestIndex.value = levelData.value.event.findIndex((e: levelEventModel) => Object.entries(e.position).length)
+
 }
 
 const createEvent = () => {
