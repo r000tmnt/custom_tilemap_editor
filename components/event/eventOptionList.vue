a98a6efc9aaae76e448dc0987b0baa0e35240445
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-06-28 10:08:17 +0800

    [Update] Import and open eventOptionEdit component

--- components/event/eventOptionList.vue
+++ components/event/eventOptionList.vue
@@ -11,26 +11,33 @@
         :scrollable="true"
         title="Dialogue options"
       ></v-card>
-      <v-btn prepend-icon="mdi-plus-box" color="primary" @click="toggleDialog('option-create')">
+      <v-btn prepend-icon="mdi-plus-box" color="primary" @click="toggleDialog('dialogue-option-create')">
             CREATE OPTION
         </v-btn>
       <v-list>
         <v-list-item v-for="(option, index) in options"
             :key="option.value"
-            @click="editOption(option)">
+            @click.stop="editOption(option)">
             {{ option.value }}
 
             <v-icon icon="mdi-trash-can" color="danger" @click.stop="deleteOption(index)"></v-icon>
         </v-list-item>
       </v-list>
     </v-dialog>
+
+    <event-option-create />
+    <event-option-edit :option="optionToEdit"
+      @edit-option="" />
 </template>
 
 <script lang="ts" setup>
-import { storeToRefs } from 'pinia';
+// import { storeToRefs } from 'pinia';
 import { ref, onMounted } from 'vue'
 import type { PropType } from 'vue'
 import type { dialogueOptionModel, eventDialogueModel } from '~/types/level'
+
+import eventOptionCreate from './eventOptionCreate.vue';
+import eventOptionEdit from './eventOptionEdit.vue';
 
 const props = defineProps({
     // options: {
@@ -51,14 +58,18 @@ const props = defineProps({
     }
 })
 
-const { editEventIndex, tileInfo } = storeToRefs(useEditorStore())
+// const { editEventIndex, tileInfo } = storeToRefs(useEditorStore())
 const { optionListDialog } = useDialogStore()
 const { toggleDialog } = useDialogStore()
 
 const options = ref<dialogueOptionModel[] | undefined>([])
 
+const optionToEdit = ref<dialogueOptionModel>()
+
 const editOption = (option: dialogueOptionModel) => {
     console.log("option :>>>", option)
+    optionToEdit.value = option
+    toggleDialog("dialouge-option-edit")
 }
 
 const deleteOption = (index: number) => {
